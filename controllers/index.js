845afa927c01cbf3a2171e0583bc3dc7f93d0d08
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-12-15 18:38:31 -0500

    added some handlebars info woirked on public js

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api')
 const homeRoutes = require('./home-routes')
+const dashboredRoutes = require('./dashbored-routes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashbored', dashboredRoutes)
 
 module.exports = router;
